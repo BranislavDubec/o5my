b75765d861fa6539e892fae705562d443f5d0911
Branislav Dubec <dubec.brano@gmail.com>
2026-08-10 16:19:34 +0200

    Fix migrations

--- migrations/0006_payment_identity_full_price.sql
+++ migrations/0006_payment_identity_full_price.sql
@@ -1,3 +1,1 @@
 ALTER TABLE payments ADD COLUMN full_price INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE payments ADD COLUMN identity TEXT;
-UPDATE payments SET full_price = amount WHERE full_price = 0;
---
+++ migrations/0008_payment_full_price_backfill.sql
@@ -0,0 +1,1 @@
+UPDATE payments SET full_price = amount WHERE full_price = 0;
