6832ca9dcc94d3405b9af9bee22ac4ce625496f1
Branislav Dubec <dubec.brano@gmail.com>
2026-09-10 22:45:23 +0200

    update app

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -217,7 +217,7 @@ export function Layout({ children }: { children: ReactNode }) {
         )}
 
         {/* Main Content */}
-        <main className="flex-1 min-h-0 overflow-y-auto overscroll-contain p-4 md:p-6 pb-28 md:pb-6 [-webkit-overflow-scrolling:touch]">
+        <main className="flex-1 min-h-0 overflow-x-hidden p-4 md:p-6 pb-28 md:pb-6 [-webkit-overflow-scrolling:touch]">
           {children}
         </main>
 
